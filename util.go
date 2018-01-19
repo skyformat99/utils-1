3d059fde0066289db7892ce66a00b7949658c9fa
fizz <864759073@qq.com>
2018-01-19 22:28:31 +0800

    升级为go style工具包

--- util.go
+++ util.go
@@ -12,27 +12,10 @@ func CheckErr(err error) {
 		panic(err)
 	}
 }
-/**
- * 类型检测 要检测的变量  期望变量类型
- */
-func TypeCheck(params interface{}, t string) bool {
-	//数据初始化
-	var (
-		return_val bool = false
-	)
-	v := reflect.ValueOf(params)
-	//获取传递参数类型
-	v_t := v.Type()
-
-	//类型名称对比
-	if v_t.String() == t {
-		return_val = true
-	}
-	return return_val
-}
 
 /**
- * 获取数据类型 (string, int, float64, []int, []string, map[string]int ...)
+ * 获取数据类型字符串 (string, int, float64, []int, []string, map[string]int ...)
+ * (能不用则不用,由于涉及到使用reflect包,性能堪忧)
  */
 func GetType(params interface{}) string {
 	//数据初始化
@@ -47,72 +30,51 @@ func GetType(params interface{}) string {
 /**
  * 给定元素值 是否在 指定的数组中
  */
-func InArray(needle interface{}, hystack []interface{}) bool {
-	for _, item := range hystack {
-		if GetType(needle) == GetType(item) {
-			if needle == item {
+func InArray(needle interface{}, hystack interface{}) bool {
+	switch key := needle.(type) {
+	case string:
+		for _, item := range hystack.([]string) {
+			if key==item {
 				return true
 			}
 		}
+	case int:
+		for _, item := range hystack.([]int) {
+			if key==item {
+				return true
+			}
+		}
+	case int64:
+		for _, item := range hystack.([]int64) {
+			if key==item {
+				return true
+			}
+		}
+	default:
+		panic("needle only support string,int,int64 type")
 	}
+	//for _, item := range hystack {
+	//	if GetType(needle) == GetType(item) {
+	//		if needle == item {
+	//			return true
+	//		}
+	//	}
+	//}
 
 	return false
 }
 
-/**
- * int 数组转 interface
- */
-func Aitoi(arr []int) []interface{} {
-	var tmp []interface{}
-
-	for _, item := range arr {
-		tmp = append(tmp, item)
-	}
-
-	return tmp
-}
-
-/**
- * string 数组转 interface
- */
-func Astoi(arr []string) []interface{} {
-	var tmp []interface{}
-
-	for _, item := range arr {
-		tmp = append(tmp, item)
-	}
-
-	return tmp
-}
-
-func ParseInt(data interface{}) int {
-	dataType := GetType(data)
-	var res int
-	switch dataType {
-	case "string":
-		res, _ = strconv.Atoi(data.(string))
-	case "int":
-		res = data.(int)
-	default:
-		panic("只能转换字符串类型")
-	}
-
-	return res
-}
-
 func ParseStr(data interface{}) string {
-	dataType := GetType(data)
-	var res string
-	switch dataType {
-	case "int":
-		res = strconv.Itoa(data.(int))
-	case "string":
-		res = data.(string)
+	switch data.(type) {
+	case int:
+		return strconv.Itoa(data.(int))
+	case int64:
+		return strconv.FormatInt(data.(int64),10)
+	case string:
+		return data.(string)
 	default:
 		panic("只能转换数字类型")
 	}
-
-	return res
 }
 
 /**
